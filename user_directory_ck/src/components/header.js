096dc79453fa7afdc79de11f54e6e23c7be77b46
Red Dragon <carsonkeller1995@hotmail.com>
2020-10-16 18:28:25 -0500

    body and header components created

---
+++ user_directory_ck/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default (props) => {
+    return (
+        <thead>
+            <tr>
+                <th scope="col">Image</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Email</th>
+                <th scope="col">DOB</th>
+            </tr>
+        </thead>)
+}
